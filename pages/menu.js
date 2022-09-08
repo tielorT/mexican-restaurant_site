62ea412b28604fcfaabf2413456e00e85fa225d4
tielorT <111721927+tielorT@users.noreply.github.com>
2022-09-07 22:40:06 -0500

    commit

--- pages/menu.js
+++ pages/menu.js
@@ -74,7 +74,7 @@ export default function Menu({}) {
                                 if(item.type === 'burrito'){
                                     return <span key={item.id}><Card title={item.title} 
                                                  description={item.description} 
-                                                 id={id}
+                                                 id={item.id}
                                                  price={item.price} 
                                                  imgUrl={item.imgUrl}
                                                  key={item.id}/></span> 
