6869c5d2297f04591b84ab659babd81b12215571
tielorT <111721927+tielorT@users.noreply.github.com>
2022-09-07 22:38:41 -0500

    commit

---
+++ pages/api/menu/[id].js
@@ -0,0 +1,10 @@
+import clientPromise from "../../../mongodb";
+
+export default async function handler(req, res) {
+  const client = await clientPromise;
+  const db = client.db("RESTAURANT");
+        const { itemId } = req.query
+        
+      const menu = await db.collection("menuItems").find({id: itemId}).toArray();
+      res.status(200).json(menu);
+}
