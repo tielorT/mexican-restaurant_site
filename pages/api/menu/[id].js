36071305d6cd8061f1fde1f6096d89a9d3ff42d3
tielorT <111721927+tielorT@users.noreply.github.com>
2022-09-07 22:45:30 -0500

    commit

--- pages/api/menu/[id].js
+++ pages/api/menu/[id].js
@@ -3,8 +3,8 @@ import clientPromise from "../../../mongodb";
 export default async function handler(req, res) {
   const client = await clientPromise;
   const db = client.db("RESTAURANT");
-        const { itemId } = req.query
+        const { id } = req.query
         
-      const menu = await db.collection("menuItems").find({id: itemId}).toArray();
+      const menu = await db.collection("menuItems").find({id: id}).toArray();
       res.status(200).json(menu);
 }
